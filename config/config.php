1f7d8303b48919a14cda80c7324f5e7fa1b71d04
Lucas Moniez <114726179+m-lcs@users.noreply.github.com>
2025-02-28 19:02:59 +0100

    Mise à jour de la configuration de la base de données pour utiliser PDO avec des options améliorées

--- config/config.php
+++ config/config.php
@@ -1,13 +1,20 @@
 <?php
-$host = "localhost";
-$dbname = "youtransfer";
-$username = "root";
-$password = "";
+$host = '127.0.0.1';
+$db = 'youtransfer';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion à la base de données : " . $e->getMessage());
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
