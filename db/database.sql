b414f189b20fe6bf47967ad9748c82b359542bee
Lucas Moniez <114726179+m-lcs@users.noreply.github.com>
2025-02-28 16:43:05 +0100

    Correction du nom de la base de données et ajustement de la longueur du champ email

--- db/database.sql
+++ db/database.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE youtransfer;
-USE evaluation_php;
+USE youtransfer;
 
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(191) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
