4206f600e3cceae774b53239e47599d827f58cac
Lucas Moniez <114726179+m-lcs@users.noreply.github.com>
2025-02-28 17:27:21 +0100

    Ajout de la colonne 'role' à la table users pour gérer les rôles des utilisateurs

--- db/database.sql
+++ db/database.sql
@@ -4,7 +4,8 @@ USE youtransfer;
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(191) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    role ENUM('user', 'admin') DEFAULT 'user'
 );
 
 CREATE TABLE files (
